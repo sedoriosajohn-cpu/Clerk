0828db8df4d84dda793758b0c78733d1e4c9c3e9
John Sedoriosa <sedoriosajohn@gmail.com>
2026-02-25 10:40:26 -0500

    Finished database
    minimal database finished, will add more inputs later

--- Clerk_Capstone.session.sql
+++ Clerk_Capstone.session.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS raw_inputs; 
+
 CREATE TABLE raw_inputs (
-    raw_id SERIAL PRIMARY KEY,
+    raw_id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT NOT NULL,
     source_type TEXT,
     received_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
